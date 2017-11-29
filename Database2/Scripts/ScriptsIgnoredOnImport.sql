50e1831850950cc41866012d6cfef82baecb3dd1
PhucND <minhphucpk.1997@gmail.com>
2017-11-29 18:38:56 +0700

    sql

---
+++ Database2/Scripts/ScriptsIgnoredOnImport.sql
@@ -0,0 +1,43 @@
+﻿
+--Syntax Error: Incorrect syntax near SELECT.
+---- Chuyển số thành chữ ( đọc Tiền )
+--CREATE FUNCTION ChuyenSoThanhChu ( @so BIGINT )
+--RETURNS NVARCHAR(200)
+--AS
+--    BEGIN
+--	
+--        DECLARE @kq NVARCHAR(200)
+--        DECLARE @ngan INT
+--        DECLARE @trieu INT
+--        DECLARE @ty INT
+--        DECLARE @donvi INT
+--        IF @so = 0
+--            SET @kq = CONCAT(@kq, N' Không ')
+--        ELSE
+--            BEGIN
+--                SET @donvi = @so % 1000
+--                SET @so /= 1000
+--                SET @ngan = @so % 1000
+--                SET @so /= 1000
+--                SET @trieu = @so % 1000
+--                SET @ty = @so / 1000
+--                IF @ty > 0
+--                    SET @kq = CONCAT(@kq, dbo.baso(@ty), N' Tỷ ')
+--                IF @trieu > 0
+--                    SET @kq = CONCAT(@kq, dbo.baso(@trieu), N' Triệu ')
+--                IF @ngan > 0
+--                    SET @kq = CONCAT(@kq, dbo.baso(@ngan), N' Nghìn ')
+--                IF @donvi > 0
+--                    SET @kq = CONCAT(@kq, dbo.baso(@donvi))
+--            END
+--			
+--        RETURN(dbo.ChuanHoaChuoi(@kq))
+--    END 
+--
+--
+--SELECT  dbo.ChuanHoaChuoi(N' Một Tỷ Một  Trăm  Mười  Một Triệu Hai  Mươi Nghìn Một  Trăm  Lẻ  Lăm ')
+--SELECT dbo.ChuyenSoThanhChu(131528794541)
+
+
+
+GO
---
+++ Database2/dbo/Functions/ChuanHoaChuoi.sql
@@ -0,0 +1,23 @@
+﻿-- CHuẩn hóa chuỗi
+CREATE FUNCTION ChuanHoaChuoi ( @string NVARCHAR(200) )
+RETURNS NVARCHAR(200)
+AS
+    BEGIN
+        DECLARE @kq NVARCHAR(200)
+        DECLARE @temp1 NVARCHAR(200)
+        DECLARE @temp2 NVARCHAR(200)
+		-- cắt sau trước cách
+        SET @kq = RTRIM(@string)
+		-- viết thường
+        SET @kq = LOWER(@kq)
+		-- Thay thế 2 dấu cách = 1 dấu
+        SET @kq = REPLACE(@kq, '  ', ' ')
+		SET @kq=LTRIM(@kq)
+		SET @kq=RTRIM(@kq)
+		-- Viết hoa chữ đầu
+        SET @temp1 = SUBSTRING(@kq, 2, LEN(@kq))
+        SET @temp2 = SUBSTRING(@kq, 1, 1)
+        SET @temp2 = UPPER(@temp2)
+        SET @kq = CONCAT(@temp2, @temp1)
+        RETURN(@kq)
+    END
